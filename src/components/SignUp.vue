e3d8e3a0dfc1fd69cef02deb6a4002c609843900
Tracy-miranja <barazatracy44@gmail.com>
2024-09-03 15:19:55 +0300

    Add homepage

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,9 +3,9 @@
     <img class="logo" src="../assets/resto-logo.png" alt="web development" />
     <h1>SignUp</h1>
     <div class="register">
-      <input type="text" placeholder="enter name" />
-      <input type="email" placeholder="enter email" />
-      <input type="password" placeholder="enter password" />
+      <input type="text" placeholder="Enter name" />
+      <input type="email" placeholder="Enter email" />
+      <input type="password" placeholder="Enter password" />
       <button>SignUp</button>
     </div>
   </div>
@@ -29,13 +29,16 @@ export default {
   width: 300px;
   align-items: center;
   margin-top: 5px;
-  padding: 5px;
+  margin-bottom: 30px;
+  padding: 15px;
+  border: 1px solid rgb(7, 252, 219);
 }
 .register button {
   width: 200px;
   margin-top: 5px;
   padding: 5px;
   border-radius: 20px;
+  background: rgb(7, 252, 219);
 }
 .container {
   display: flex;
