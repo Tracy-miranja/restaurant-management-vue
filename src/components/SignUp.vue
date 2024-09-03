2999de3246ccc6b126469d65c71605f811902b85
Tracy-miranja <barazatracy44@gmail.com>
2024-09-03 09:33:59 +0300

    Add input form

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,11 +1,13 @@
 <template>
-  <img class="logo" src="../assets/resto-logo.png" alt="web development" />
-  <h1>SignUp</h1>
-  <div>
-    <input type="text" placeholder="enter name" />
-    <input type="email" placeholder="enter email" />
-    <input type="password" placeholder="enter password" />
-    <button>SignUp</button>
+  <div class="container">
+    <img class="logo" src="../assets/resto-logo.png" alt="web development" />
+    <h1>SignUp</h1>
+    <div class="register">
+      <input type="text" placeholder="enter name" />
+      <input type="email" placeholder="enter email" />
+      <input type="password" placeholder="enter password" />
+      <button>SignUp</button>
+    </div>
   </div>
 </template>
 <script>
@@ -17,5 +19,28 @@ export default {
 .logo {
   width: 100px;
   /* height: 10vh; */
+}
+.register {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.register input {
+  width: 300px;
+  align-items: center;
+  margin-top: 5px;
+  padding: 5px;
+}
+.register button {
+  width: 200px;
+  margin-top: 5px;
+  padding: 5px;
+  border-radius: 20px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
