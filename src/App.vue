35a2ac05959317074f70404c4483d1530068ba01
Tracy-miranja <barazatracy44@gmail.com>
2024-09-03 09:23:39 +0300

    Initialise project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <signup />
+
+  <main></main>
+</template>
+<script>
+import Signup from "./components/Signup.vue";
+export default {
+  name: "App",
+  components: {
+    Signup,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,21 @@
+<template>
+  <img class="logo" src="../assets/resto-logo.png" alt="web development" />
+  <h1>SignUp</h1>
+  <div>
+    <input type="text" placeholder="enter name" />
+    <input type="email" placeholder="enter email" />
+    <input type="password" placeholder="enter password" />
+    <button>SignUp</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+<style scoped>
+.logo {
+  width: 100px;
+  /* height: 10vh; */
+}
+</style>
